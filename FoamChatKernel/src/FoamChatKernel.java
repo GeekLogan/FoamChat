87c951fc376f49af4d4068fbd52ef889e4351057
Logan Walker <logan.walker@me.com>
2015-11-14 13:14:23 -0500

    Update Kernel Constructor

--- FoamChatKernel/src/FoamChatKernel.java
+++ FoamChatKernel/src/FoamChatKernel.java
@@ -1,13 +1,30 @@
+
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Logan Walker <logan.walker@me.com>
  *
  */
 public class FoamChatKernel {
-    
+    List<NodeReference> nodes;
     
     public FoamChatKernel() {
-        //Do Nothing Right Now
+        
+        nodes = new ArrayList<>();
+        try {
+            for( String ip : IPTools.getCurrentIP() ) {
+                nodes.add( new NodeReference( ip ) );
+            }
+        } catch (SocketException ex) {
+            System.err.println("No IP Interfaces Found!");
+            System.exit( -1 );
+        }
+        
         
         
     }
