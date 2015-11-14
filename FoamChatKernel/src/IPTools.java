9a37bbb151d6418094764bf72f688efe9d8f247e
Logan Walker <logan.walker@me.com>
2015-11-14 11:52:31 -0500

    Update IPTools

--- FoamChatKernel/src/IPTools.java
+++ FoamChatKernel/src/IPTools.java
@@ -1,8 +1,32 @@
+
+import java.net.InetAddress;
+
 /**
  *
  * @author Logan Walker <logan.walker@me.com>
  * 
  */
 public class IPTools {
+    
+    /**
+     * 
+     * Modified from http://crunchify.com/how-to-get-server-ip-address-and-hostname-in-java/
+     * 
+     * @return the IP address of current system in a String
+     */
+    public static String getCurrentIP() {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip + "";
+    }
     
 }
