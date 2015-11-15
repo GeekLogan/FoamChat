2dfb3148f69a223987d49582529c92e0a4654cd9
SuperPichu <chrismidkiff15@gmail.com>
2015-11-14 19:55:42 -0500

    Fixed File Transfer

--- FoamChatKernel/src/FileClient.java
+++ FoamChatKernel/src/FileClient.java
@@ -5,9 +5,9 @@ import java.net.Socket;
  * Created by chris on 11/14/15.
  */
 public class FileClient {
-    private static final String ip = "25.16.95.241";
+    private static final String ip = "127.0.0.1";
     private static final int port = 3248;
-    private static final String out = "/Users/logan/Desktop/file.mkv";
+    private static final String out = "/home/chris/dev/tmp.mkv";
 
     public static void main(String[] args){
         byte[] aByte = new byte[1];
@@ -23,7 +23,6 @@ public class FileClient {
 
         }
 
-        //ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
         if (inputStream != null) {
 
@@ -32,14 +31,12 @@ public class FileClient {
             try {
                 fos = new FileOutputStream( out );
                 bufferedOutputStream = new BufferedOutputStream(fos);
-                bytesRead = inputStream.read(aByte, 0, aByte.length);
 
                 do {
                     bufferedOutputStream.write(aByte);
                     bytesRead = inputStream.read(aByte);
                 } while (bytesRead != -1);
 
-                //bufferedOutputStream.write(byteArrayOutputStream.toByteArray());
                 bufferedOutputStream.flush();
                 bufferedOutputStream.close();
                 socket.close();
