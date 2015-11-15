1d9a65c6ac942975ebb7b77e800da4aca5579d4e
Logan Walker <logan.walker@me.com>
2015-11-15 03:31:04 -0500

    Create OS X Port Template

---
+++ FoamChat-OSX/FoamChat/FoamChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FoamChat
+//
+//  Created by Logan Walker on 11/15/15.
+//  Copyright (c) 2015 FoamChatDevelopers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
